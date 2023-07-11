89055aea8ccc2026a234a5e65c4ba65403cb8ddc
Justin Merrell <merrelljustin@gmail.com>
2023-07-11 12:26:54 -0400

    Update setup.sh

--- builder/setup.sh
+++ builder/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade -y
+# Update System
+apt-get update && apt-get upgrade -y
+
 
 # Install System Dependencies
-apt-get install -y software-properties-common python3
+# - openssh-server: for ssh access and web terminal
+apt-get install -y \
+    software-properties-common \
+    python3 \
+    openssh-server
 
-apt-get autoremove -y
-apt-get clean -y
-rm -rf /var/lib/apt/lists/*
+
+# Clean up
+apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+
+
+# Install Python Dependencies
+# - pip: for installing python packages
+python get-pip.py
