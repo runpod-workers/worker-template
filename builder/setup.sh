b16f8d9f416296008c377e0217002d7fbe978efa
Justin Merrell <merrelljustin@gmail.com>
2023-07-11 14:00:04 -0400

    Update setup.sh

--- builder/setup.sh
+++ builder/setup.sh
@@ -12,8 +12,12 @@ apt-get install -y --no-install-recommends software-properties-common
 
 # Install Python 3.10
 add-apt-repository ppa:deadsnakes/ppa -y
-apt-get update && apt-get install -y --no-install-recommends python3.10 python3.10-dev python3.10-distutils python3-pip
+apt-get update && apt-get install -y --no-install-recommends python3.10 python3.10-dev python3.10-distutils
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+
+# Install pip for Python 3.10
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 
 # Clean up
 apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
