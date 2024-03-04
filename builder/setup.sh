a90bf459d688d45a30e247e2495ee855aa927aed
srikanthganta182 <srikanth.ganta@ixor.be>
2024-03-04 20:03:31 +0100

    Update setup.sh script to install Python 3.10 and clean up unnecessary packages

--- builder/setup.sh
+++ builder/setup.sh
@@ -4,26 +4,26 @@
 #       If you use a custom base image you can add your required system dependencies here.
 
 # set -e # Stop script on error
-# apt-get update && apt-get upgrade -y # Update System
+apt-get update && apt-get upgrade -y # Update System
 
 # Install System Dependencies
 # - openssh-server: for ssh access and web terminal
-# apt-get install -y --no-install-recommends software-properties-common curl git openssh-server
+apt-get install -y --no-install-recommends software-properties-common curl git openssh-server
 
 apt install ffmpeg
 # Install Python 3.10
 
 # add-apt-repository ppa:deadsnakes/ppa -y
-# apt-get update && apt-get install -y --no-install-recommends python3.10 python3.10-dev python3.10-distutils
-# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+apt-get update && apt-get install -y --no-install-recommends python3.10 python3.10-dev python3.10-distutils
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 
 # # Install pip for Python 3.10
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# python3 get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 
 # Clean up, remove unnecessary packages and help reduce image size
 apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
-sudo rm -rf /usr/share/dotnet
-sudo rm -rf /opt/ghc
-sudo rm -rf "/usr/local/share/boost"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+# sudo rm -rf /usr/share/dotnet
+# sudo rm -rf /opt/ghc
+# sudo rm -rf "/usr/local/share/boost"
+# sudo rm -rf "$AGENT_TOOLSDIRECTORY"
